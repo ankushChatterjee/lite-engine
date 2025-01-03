39ca80ab85b44975b7b0b0dea46e633a11619a58
Ankush Chatterjee <ankush.chatterjee@harness.io>
2025-01-03 12:22:36 +0530

    check2

--- pipeline/runtime/run.go
+++ pipeline/runtime/run.go
@@ -123,9 +123,8 @@ func executeRunStep(ctx context.Context, f RunFunc, r *api.StartStepRequest, out
 		if reportSaveErr == nil && report.TestSummaryAsOutputEnabled(r.Envs) {
 			log.Infof("Test summary set as output variables")
 		}
-	} else {
-		log.Infof("No test summary set")
 	}
+
 	summaryOutputsV2 := report.GetSummaryOutputsV2(summaryOutputs, r.Envs)
 
 	if exited != nil && exited.Exited && exited.ExitCode == 0 {
